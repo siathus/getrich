7e39dea3a70b8df8e30e773b7fe072e150563500
KUvH <kuvh@live.co.kr>
2016-11-24 21:39:09 +0900

    Game Intro

--- EntryPoint.cpp
+++ EntryPoint.cpp
@@ -1,9 +1,37 @@
 #include <iostream>
+#include <Windows.h>
+#include <conio.h>
 
 using namespace std;
 
 int main() {
-	cout << "Hello World!" << endl;
+	cout << "..........`````````````````````````````  ````..` ````....````.::.....````...-----........-----::::::" << endl;
+	cout << "............``````````````````````````` ```.::+/::-.-...:--.-/+-....```...-----........------:::::--" << endl;
+	cout << "..............````````````````````````.-://ooo//++ooooo+/+o////-..```....----........-----:::::-----" << endl;
+	cout << "................```````````````:+/:..-++ooo/+o++o++++oo++++:-//..---:--::--........----:::::--------" << endl;
+	cout << "..................```````````````./ooosoooo+o++++/++++ooossoo++/:+---/+:--.`.....----::::-----------" << endl;
+	cout << "........```..........```````..///:/+oso+ooo+++/++++++/:/oosss+o/o+:.------.```..---:::-----------:::" << endl;
+	cout << "---........```.........``````/++:////sooo+o+o+++++++ooo+oossysso/++:---.:.````...-------------::::::" << endl;
+	cout << "-------......````.......```.-+sso+++++////:::-::::-:-::-:::///////:/+:.``    ``......------:::::::::" << endl;
+	cout << ".---------.....``````.....:o/+o+oso+sooo+so/+..o://o:s-`+ss/os/oo/+--.``     ``....----:::::::::::::" << endl;
+	cout << "....-:+oosssssoo+/-`````..-s++s+o+o+++//:::::::::/+osssssssoo+///:/::....   ``....--::::::::::::::::" << endl;
+	cout << "..-+ss+:--...---:+ys+osssssyyyyyyyyyyyyyyyyyyyyys+:--------:+syysooooo++/://ossssoooyyys/:::+syyso::" << endl;
+	cout << "./yo:....-://:-..-yy/-..````..-++............+hs.```.----.````/ys-`.oys+-.`` ```.:sy-.:yh+:oh+--+hy/" << endl;
+	cout << ":ys....+hhhhysyyhhho```/oooo++/yy++++.```/+++yhs```-hdddddy```.ys```oy-   `:+o+/:/hs.`.yhysyh:..:dho" << endl;
+	cout << "+yo...:yyyy/.```.:so```-------/yyyyyy-``-hyyyyhs```-+-.....```:hy.``oo```:hyyyyyhhhs.`.o:::::-..:dho" << endl;
+	cout << "+ys....oyyyhyy/.``/o```::::::/ohyyyyy-``-hyyyyhs```-o-....````:hy.``os```:yyyyyyyhhs.`.o/::::...:dh+" << endl;
+	cout << "-hhs-...-:///:.```+o```://///:-/yhhhy-``-hhhhhhs```:hdddddh.```yy.``oh+.``.:///:--+s.`.yddddd:..:dh/" << endl;
+	cout << "./hhhs+:-......:+yhho:-.....--/shdhhho..+hdhyhhy-``+hdhyyhh/``-hh:.-yhdhs/:-..--:/yy:-:hhhshho--+dh/" << endl;
+	cout << "--:shddddddddddddddddddddddddddddy/+ddddddy/-:hddddddy/--sddddddddddddhdddddddddddddddddd+-+ddddddh:" << endl;
+	cout << "-----/osyhhhhhhys+:-/osyhhhhyyso/-..:oyyyo:-..-oyhhhs/-..-/yhhhysyhhyo::+osyyhhyysooyyys+:--/oyhyo:." << endl;
+	cout << "...................................................................................................." << endl;
+	cout << "...................................忙式式式式式式式式式式式式式忖..................................." << endl;
+	cout << "...................................弛Press Any Key To Continue!弛..................................." << endl;
+	cout << "...................................戌式式式式式式式式式式式式式戎..................................." << endl; 
+	cout << "...................................................................................................." << endl;
+
+	int ch = _getch();
+	//Game Manager Call
 
 	return 0;
 }
